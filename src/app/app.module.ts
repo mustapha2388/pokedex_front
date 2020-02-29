594b4c111fcf6a70d1f9b9bd5ef840c1c02450a0
mouss <mouss2388@gmail.com>
2020-02-29 23:26:02 +0100

    Update Project
    + Pagination, Forms, HttpClient, Pokemon.ts & Page.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListPokemonComponent } from './components/list-pokemon/list-pokemon.component';
-
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,10 @@ import { ListPokemonComponent } from './components/list-pokemon/list-pokemon.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PaginationModule.forRoot(),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -1,2 +1,11 @@
 export class Pokemon {
+    constructor(
+        public id: number,
+        public  name: string,
+        public  type1: string,
+        public  type2: string,
+        public  description: string,
+        private weight: number,
+        private height: number
+    ) {}
 }
