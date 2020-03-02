20161188e45270ffe969b5a38af735b1eb166fd9
mouss <mouss2388@gmail.com>
2020-03-02 22:55:52 +0100

    Put img on navbar

--- src/app/components/list-pokemon/list-pokemon.component.ts
+++ src/app/components/list-pokemon/list-pokemon.component.ts
@@ -24,15 +24,28 @@ export class ListPokemonComponent implements OnInit {
   pagePokemon: Page<Pokemon>;
   private pokemonSubscription: Subscription;
 
+  tagFilter: string;
+
   ngOnInit() {
     this.noPage = 1;
     this.taillePage = 8;
     this.totalItems = 0;
     this.pagePokemon = Page.emptyPage<Pokemon>();
     this.pokemonService.refreshList(this.router.url);
+    this.tagFilter = 'numéro';
     this.getPokemon();
+    this.changeTag();
   }
 
+  convertTypeToImg(type: string): string {
+    switch (type) {
+      case 'Grass':
+        return './assets/img/grass.png';
+        case 'Poison':
+          return './assets/img/grass.png';
+
+    }
+  }
   getPokemon() {
     this.pokemonSubscription = this.pokemonService
       .getPokemonPageAsObsversable()
@@ -44,6 +57,29 @@ export class ListPokemonComponent implements OnInit {
       });
   }
 
+  public changeTag() {
+    if (this.router.url.includes('min/weight')) {
+      this.tagFilter = 'Pokemon(s) le(s) plus léger';
+    } else if (this.router.url.includes('max/weight' )) {
+      this.tagFilter = 'Pokemon(s) le(s) plus lourd';
+
+    } else if (this.router.url.includes('min/height' )) {
+      this.tagFilter = 'Pokemon(s) le(s) plus petit';
+
+    } else if (this.router.url.includes('max/height' )) {
+      this.tagFilter = 'Pokemon(s) le(s) plus grand';
+
+    } else if (this.router.url.includes('list-id-desc' )) {
+      this.tagFilter = 'Pokemons par # décroissant';
+    }  else if (this.router.url.includes('list-desc' )) {
+      this.tagFilter = 'Pokemons par nom Z-A';
+    } else if (this.router.url.includes('list-asc' )) {
+      this.tagFilter = 'Pokemons par nom A-Z';
+    } else {
+      this.tagFilter = 'Pokemons par # croissant';
+    }
+
+  }
   public onPageChanged(event): void {
     this.pokemonService.setNoPage(event.page - 1, this.router.url);
   }
